51c33a9796962eb38ea20c39fe3beb8fe25ab90d
Денис <kornejchukdenis@gmail.com>
2016-11-29 02:43:21 +0200

    Расширение БД

---
+++ console/migrations/m161129_002010_db_exp.php
@@ -0,0 +1,51 @@
+<?php
+
+use yii\db\Migration;
+
+class m161129_002010_db_exp extends Migration
+{
+    public function up()
+    {
+        $tableOptions = null;
+        if ($this->db->driverName === 'mysql') {
+            $tableOptions = 'CHARACTER SET utf8 COLLATE utf8_unicode_ci ENGINE=InnoDB';
+        }
+
+        $this->createTable('common_info', [
+            'id' => $this->primaryKey(),
+            'info_header' => $this->char(50)->notNull(),
+            'info_content' => $this->text()->null()
+        ],$tableOptions);
+
+        $this->createTable('projects', [
+            'id' => $this->primaryKey(),
+            'title' => $this->char(50)->null(),
+            'description' => $this->text(),
+            'sort_order' => $this->integer()->unsigned()->null(),
+            'complete_date' => $this->dateTime()->null(),
+            'link' => $this->char(200)
+        ], $tableOptions);
+
+        $this->createTable('contacts', [
+            'id' => $this->primaryKey(),
+            'title' => $this->char(50)->notNull(),
+            'description' => $this->text()->null(),
+        ], $tableOptions);
+
+        $this->createTable('achievements', [
+            'id' => $this->primaryKey(),
+            'title' => $this->char(50)->notNull(),
+            'content' => $this->text()->null(),
+            'time' => $this->dateTime()->null()
+        ], $tableOptions);
+    }
+
+    public function down()
+    {
+        $this->dropTable('achievements');
+        $this->dropTable('contacts');
+        $this->dropTable('projects');
+        $this->dropTable('common_info');
+        return true;
+    }
+}
