811b17722429ae50b8722fedb971d41b5fb13e9d
Денис <kornejchukdenis@gmail.com>
2016-11-29 08:32:03 +0200

    Фикс отображений

--- backend/views/site/index.php
+++ backend/views/site/index.php
@@ -6,8 +6,7 @@ $this->title = 'My Yii Application';
 
     <div class="jumbotron">
         <h1>Congratulations!</h1>
-        <?=Yii::getAlias("@storage/")?>
-        <img src="<?=Yii::getAlias("@storage/")?>lol.jpg" alt="">
+
         <p class="lead">You have successfully created your Yii-powered application.</p>
 
         <p><a class="btn btn-lg btn-success" href="http://www.yiiframework.com">Get started with Yii</a></p>
