4a41c4694175b8adb46276a337c0f105fcdc819d
Denys Korniichuk <kornejchukdenis@gmail.com>
2017-02-23 02:13:35 +0200

    Activity stats fix

--- backend/controllers/ActivityController.php
+++ backend/controllers/ActivityController.php
@@ -3,9 +3,11 @@
 namespace backend\controllers;
 
 use common\models\Activity;
+use Yii;
 use yii\base\Controller;
 use yii\db\Query;
 use yii\filters\AccessControl;
+use yii\web\Response;
 
 class ActivityController extends Controller
 {
@@ -28,12 +30,16 @@ class ActivityController extends Controller
     public function actionIndex()
     {
         /* SELECT MONTH(activity.date) as month_num, MONTHNAME(activity.date) as month, count(YEAR(activity.date)) as visits FROM activity GROUP BY month ORDER BY month_num LIMIT 12; */
-        $res = (new Query())->select('MONTH(activity.date) as month_num, MONTHNAME(activity.date) as month, count(YEAR(activity.date)) as visits')->from('activity')->groupBy('month')->orderBy('month_num')->limit(12)->all();
-        $data = "['Month','visits'],";
+        $res = (new Query())->select('MONTH(activity.date) as month_num, MONTHNAME(activity.date) as month, count(YEAR(activity.date)) as visits')->from('activity')->groupBy('month, month_num')->orderBy('month_num')->limit(12)->all();
+        /*$data = "['Month','visits'],";
         foreach ($res as $item) {
             $data .= "['".$item['month']."',".$item['visits']."],";
         }
         $data =  substr($data, 0, -1);
-        return $this->render('index',['diagrammData' => $data]);
+        return $this->render('index',['diagrammData' => $data]);*/
+
+        Yii::$app->response->format = Response::FORMAT_JSON;
+        return $res;
+
     }
 }
